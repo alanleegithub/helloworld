c1b751daf16d4441c8ba6ce13ee33d74aae0d1cf
alan.lee <alanlee2005@gmail.com>
2014-07-17 17:24:31 +0800

    [Study] Hello World C - url dispatcher

--- HelloWorld/urls.py
+++ HelloWorld/urls.py
@@ -1,14 +1,18 @@
 from django.conf.urls import patterns, include, url
-from HelloWorldApp.views import foo
+from HelloWorldApp.views import foo, home
 
 #from django.contrib import admin
 #admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'HelloWorld.views.home', name='home'),
+    url(r'^$', foo, name='home123'),
     # url(r'^blog/', include('blog.urls')),
 
     #url(r'^admin/', include(admin.site.urls)),
     url(r'HelloWorldApp/$', foo),
+)
+
+urlpatterns += patterns('',
+    url(r'^places/(?P<name>\w+)/$', home, name='places.view_place')
 )
--- HelloWorldApp/views.py
+++ HelloWorldApp/views.py
@@ -6,3 +6,10 @@ def foo(request,):
     return render_to_response("helloDJ/home.html",
                                {"Testing" : "Django Template Inheritance ",
                                "HelloHello" : "Hello World - Django"})
+
+
+def home(request, name):
+    return render_to_response("helloDJ/home.html",
+                               {"Testing" : "Django Template Inheritance ",
+                               "HelloHello" : "Hello World " + name + " - Django"})
+    #place = models.Place.objects.get(name__iexact=name)
