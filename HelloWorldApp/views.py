79400b6ea88508fd5973c6c43731291613e23936
alan.lee <alanlee2005@gmail.com>
2014-07-17 18:03:05 +0800

    [Study] Hello World D - Models and SQLite Database

--- HelloWorldApp/views.py
+++ HelloWorldApp/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render_to_response
+from models import Line
+
 def foo(request,):
     return render_to_response("helloDJ/home.html",
-                               {"Testing" : "Django Template Inheritance ",
-                               "HelloHello" : "Hello World - Django"})
+                               {"lines" : Line.objects.all()})
 
 
 def home(request, name):
