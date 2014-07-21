5e8a793879458d333ab116800d8d7db551cb3a64
alan.lee <alanlee2005@gmail.com>
2014-07-21 13:51:42 +0800

    [Study] Image upload sample code - local host

--- HelloWorld/urls.py
+++ HelloWorld/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import patterns, include, url
 from HelloWorldApp.views import foo, home
 from blog.views import HelloTemplate
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -15,7 +18,8 @@ urlpatterns = patterns('',
     url(r'^hello_template/$', 'blog.views.hello_template'),
     url(r'^hello_template_simple/$', 'blog.views.hello_template_simple'),
     url(r'^hello_class_view/$', HelloTemplate.as_view()),
-)
+    url(r'^myapp/', include('myapp.urls')),
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += patterns('',
     url(r'^places/(?P<name>\w+)/$', home, name='places.view_place')
